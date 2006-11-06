43b4dfcf8390fdf2bc2118c4e84da64636665b95
William Ahern <william@25thandClement.com>
2006-11-06 04:06:04 +0000

    Some changes

--- com/string.h
+++ com/string.h
@@ -1,9 +1,7 @@
 #ifndef LIBCOM_COM_STRING_H
 #define LIBCOM_COM_STRING_H
 
-#include <com/config.h>	/* LIBCOM_SCOPE */
-
-#include <stddef.h>	/* size_t */
+#include <string.h>	/* strcmp(3) */
 
 
 #if !defined STRINGIFY
@@ -12,11 +10,26 @@
 #endif
 
 
+#if !defined streq
+#define streq(a, b)		(0 == strcmp((a), (b)))
+#endif
+
+#if !defined strneq
+#define strneq(a, b, z)		(0 == strncmp((a), (b), (z)))
+#endif
+
+#if !defined strcaseeq
+#define strcaseeq(a, b)		(0 == strcasecmp((a), (b)))
+#endif
+
+#if !defined strncaseeq
+#define strncaseeq(a, b, z)	(0 == strncasecmp((a), (b), (z)))
+#endif
+
+
 #include <com/strlcpy.h>
+
 #include <com/strlcat.h>
-
-#
-
 
 
 #endif /* LIBCOM_COM_STRING_H */
