cd657d18474cc57a77596ffca810ff46ff87860c
William Ahern <william@25thandClement.com>
2006-11-12 22:33:46 +0000

    more changes

--- com/errno.h
+++ com/errno.h
@@ -14,4 +14,24 @@
 #endif
 
 
+/*
+ * Some BSD's have EOPNOTSUPP but not ENOTSUP. Let's make sure we have both.
+ */
+#if !defined ENOTSUP
+#if defined EOPNOTSUPP
+#define ENOTSUP		EOPNOTSUPP
+#else
+#define ENOTSUP		EINVAL
+#endif
+#endif /* !defined ENOTSUP */
+
+#if !defined EOPNOTSUPP
+#if defined ENOTSUP
+#define EOPNOTSUPP	ENOTSUP
+#else
+#define EOPNOTSUPP	EINVAL
+#endif
+#endif /* !defined EOPNOTSUPP */
+
+
 #endif /* LIBCOM_COM_ERRNO_H */
--- com/socket.h
+++ com/socket.h
@@ -1,10 +1,12 @@
 #ifndef LIBCOM_COM_SOCKET_H
 #define LIBCOM_COM_SOCKET_H
 
+#include <errno.h>		/* errno */
 
 #if !_WIN32
-#define INVALID_SOCKET	-1
-#define closesocket(fd)	close((fd))
+#define INVALID_SOCKET		-1
+#define closesocket(fd)		close((fd))
+#define WSAGetLastError()	errno
 #endif
 
 
