9d525b0a30ff1d98dc8573b69a2d459851abf067
William Ahern <william@25thandClement.com>
2007-03-18 07:16:12 +0000

    non-standardized stuff

---
+++ nostd/STRINGIFY.h
@@ -0,0 +1,11 @@
+#ifndef STDC_STRINGIFY_H
+#define STDC_STRINGIFY_H
+
+
+#if !defined STRINGIFY
+#define STRINGIFY_(s)	#s
+#define STRINGIFY(s)	STRINGIFY_(s)
+#endif
+
+
+#endif /* STDC_STRINGIFY_H */
---
+++ nostd/arraylen.h
@@ -0,0 +1,17 @@
+#ifndef STDC_ARRAYLEN_H
+#define STDC_ARRAYLEN_H
+
+
+#if !defined arraylen
+#if __GNUC__
+#define arraylen(a)
+	(sizeof(a) / sizeof((a)[0])						\
+	sizeof(typeof(int[1 - 2*!!__builtin_types_compatible_p(typeof(a),	\
+	typeof(&a[0]))]))*0)
+#else
+#define arraylen(a, b)	(sizeof (a) / sizeof (a)[0])
+#endif /* __GNUC__ */
+#endif /* arraylen */
+
+
+#endif /* STDC_ARRAYLEN_H */
