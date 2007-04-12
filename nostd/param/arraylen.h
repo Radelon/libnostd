8cb9ef501249e58eeffcc08f0b32c0f62b9db24b
William Ahern <william@25thandClement.com>
2007-04-12 17:29:48 +0000

    another typo

--- nostd/param/arraylen.h
+++ nostd/param/arraylen.h
@@ -9,7 +9,7 @@
 	sizeof(typeof(int[1 - 2*!!__builtin_types_compatible_p(typeof(a),	\
 	typeof(&a[0]))]))*0)
 #else
-#define arraylen(a, b)	(sizeof (a) / sizeof (a)[0])
+#define arraylen(a)	(sizeof (a) / sizeof (a)[0])
 #endif /* __GNUC__ */
 #endif /* arraylen */
 
