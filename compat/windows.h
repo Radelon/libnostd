c7ecd632a2202c4962edadd31a90b16b622d646c
William Ahern <william@25thandClement.com>
2007-05-04 04:57:56 +0000

    forgot about win32

---
+++ compat/windows.h
@@ -0,0 +1,6 @@
+#ifndef COMPAT_WINDOWS_H
+#define COMPAT_WINDOWS_H
+
+#include <win32/windows.h>
+
+#endif /* !COMPAT_WINDOWS_H */
---
+++ compat/winsock2.h
@@ -0,0 +1,6 @@
+#ifndef COMPAT_WINSOCK2_H
+#define COMPAT_WINSOCK2_H
+
+#include <win32/winsock2.h>
+
+#endif /* !COMPAT_WINSOCK2_H */
