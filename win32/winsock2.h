168fc2a2e28e417ccc1fa359fb967c7333a83c4e
William Ahern <william@25thandClement.com>
2008-08-25 05:35:09 +0000

    simplify winsock2 compat

--- win32/winsock2.h
+++ win32/winsock2.h
@@ -1,15 +1,52 @@
 #ifndef WIN32_WINSOCK2_H
 #define WIN32_WINSOCK2_H
 
-
-#include <win32/winsock2/INVALID_SOCKET.h>
-#include <win32/winsock2/WSAGetLastError.h>
-#include <win32/winsock2/closesocket.h>
+#include <errno.h>
 
 
 #if HAVE_WINSOCK2_H || _WIN32
+#if defined(_MSC_VER)
+#include <winsock2.h>
+#else
 #include_next <winsock2.h>
 #endif
+#endif
+
+
+#if !defined(HAVE_WSAGETLASTERROR)
+#if _WIN32
+#define HAVE_WSAGETLASTERROR	1
+#else
+#define HAVE_WSAGETLASTERROR	0
+#endif
+#endif
+
+#if !HAVE_WSAGETLASTERROR
+#define WSAGetLastError()	errno
+#endif
+
+
+#if !defined(INVALID_SOCKET)
+#define INVALID_SOCKET	(-1)
+#endif
+
+
+#if !defined(HAVE_CLOSESOCKET)
+#if _WIN32
+#define HAVE_CLOSESOCKET	1
+#else
+#define HAVE_CLOSESOCKET	0
+#endif
+#endif
+
+
+#if !HAVE_CLOSESOCKET
+
+#include <unistd.h>	/* close(2) */
+
+#define closesocket(s)	close((s))
+
+#endif /* !HAVE_CLOSESOCKET */
 
 
 #endif /* WIN32_WINSOCK2_H */
