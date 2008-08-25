494aafc7f82a5421abb38895b4526c71a6ed28ca
William Ahern <william@25thandClement.com>
2008-08-25 05:19:14 +0000

    simplify posix errno compat

--- posix/errno.h
+++ posix/errno.h
@@ -1,9 +1,30 @@
-#ifndef SUS_ERRNO_H
-#define SUS_ERRNO_H
+#ifndef POSIX_ERRNO_H
+#define POSIX_ERRNO_H
 
-#include <posix/errno/ENOTSUP.h>
-#include <posix/errno/EOPNOTSUPP.h>
 
+#if defined(_MSC_VER)
+#include <errno.h>
+#else
 #include_next <errno.h>
+#endif
 
-#endif /* SUS_ERRNO_H */
+
+#if !defined ENOTSUP
+#if defined EOPNOTSUPP
+#define ENOTSUP		EOPNOTSUPP
+#else
+#define ENOTSUP		EINVAL
+#endif
+#endif /* !defined ENOTSUP */
+
+
+#if !defined EOPNOTSUPP
+#if defined ENOTSUP
+#define EOPNOTSUPP	ENOTSUP
+#else
+#define EOPNOTSUPP	EINVAL
+#endif
+#endif /* !defined EOPNOTSUPP */
+
+
+#endif /* POSIX_ERRNO_H */
