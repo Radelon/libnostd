1450e74697c4580180987d7d694652d2648392d3
William Ahern <william@25thandClement.com>
2008-08-25 06:02:04 +0000

    billib is the new repo for random useful stuff

--- nostd/sys/param.h
+++
@@ -1,9 +0,0 @@
-#ifndef NOSTD_SYS_PARAM_H
-#define NOSTD_SYS_PARAM_H
-
-#include <nostd/sys/param/PP_NARG.h>
-#include <nostd/sys/param/STRINGIFY.h>
-#include <nostd/sys/param/alignof.h>
-#include <nostd/sys/param/arraylen.h>
-
-#endif /* !NOSTD_SYS_PARAM_H */
