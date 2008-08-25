fecc90357c3a998b9b2102fb0b8215b69076270c
William Ahern <william@25thandClement.com>
2008-08-25 05:25:41 +0000

    simplify win32 errno compat

--- win32/windows.h
+++ win32/windows.h
@@ -2,12 +2,38 @@
 #define WIN32_WINDOWS_H
 
 
-#include <win32/windows/GetLastError.h>
-#include <win32/windows/SetLastError.h>
+#if !defined(HAVE_GETLASTERROR)
+#if _WIN32
+#define HAVE_GETLASTERROR	1
+#else
+#define HAVE_GETLASTERROR	0
+#endif
+#endif
+
+#if !HAVE_GETLASTERROR
+#define GetLastError()	errno
+#endif
+
+
+#if !defined(HAVE_SETLASTERROR)
+#if _WIN32
+#define HAVE_SETLASTERROR	1
+#else
+#define HAVE_SETLASTERROR	0
+#endif
+#endif
+
+#if !HAVE_SETLASTERROR
+#define SetLastError(e)	(errno = (e))
+#endif
 
 
 #if HAVE_WINDOWS_H || _WIN32
+#if defined(_MSC_VER)
+#include <windows.h>
+#else
 #include_next <windows.h>
+#endif
 #endif
 
 
