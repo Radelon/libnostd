36cb11c9c5cecae57caafc304a06dbf73c228248
William Ahern <william@25thandClement.com>
2010-02-25 01:47:32 +0000

    add setproctitle

---
+++ bsd/stdlib/setproctitle.h
@@ -0,0 +1,29 @@
+#ifndef BSD_STDLIB_SETPROCTITLE_H
+#define BSD_STDLIB_SETPROCTITLE_H
+
+#ifndef HAVE_SETPROCTITLE
+#if defined __FreeBSD__ \
+ || defined __NetBSD__  \
+ || defined __APPLE__
+#define HAVE_SETPROCTITLE 1
+#else
+#define HAVE_SETPROCTITLE 0
+#endif
+#endif
+
+
+#if !HAVE_SETPROCTITLE
+
+#if __GNUC__
+
+void setproctitle(const char *, ...) __attribute__((format (printf 1, 2)));
+
+#endif
+
+void setproctitle(const char *, ...);
+
+
+#endif /* !HAVE_SETPROCTITLE */
+
+
+#endif /* BSD_STDLIB_SETPROCTITLE_H */
